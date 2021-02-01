18b52ed0d1b935359990e1b31501611a0bddb569
yinhongming2018@163.com <yinhongming2018@163.com>
2021-02-01 18:29:21 +0800

    1

--- src/main/java/cn/veg/gateway/VegGatewayApplication.java
+++ src/main/java/cn/veg/gateway/VegGatewayApplication.java
@@ -1,8 +1,7 @@
-package cn.veg;
+package cn.veg.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
